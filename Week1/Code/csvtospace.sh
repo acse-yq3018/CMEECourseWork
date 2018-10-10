a07d8cb34b23122375cff5f1e74741c16aa51048
yq3018 <yq3018@imperial.ac.uk>
2018-10-10 12:06:17 +0100

    Week1

--- Week1/Code/csvtospace.sh
+++ Week1/Code/csvtospace.sh
@@ -2,7 +2,7 @@
 # Author:Yuxin Qin yq3018@imperial.ac.uk
 # Script: csvtospace.sh
 # Desc: substitute the commas in the files with tabs
-# Arguments: 1-> tab delimited file
+# Arguments: bash csvtospace.sh ../Data/Temperatures/1800.csv   bash csvtospace.sh ../Data/Temperatures/1801.csv    bash csvtospace.sh ../Data/Temperatures/1802.csv      bash csvtospace.sh ../Data/Temperatures/1803.csv
 # Date: Oct 2018
 
 echo "Creating a tabs delimited version of $1 ..."
